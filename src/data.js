f75969cca348c0ccf318c55cc6324af06a57814e
Kevendra Singh Parihar <kevendrasp27@gmail.com>
2025-11-08 04:51:15 +0530

    first commit

---
+++ src/data.js
@@ -0,0 +1,81 @@
+// ==========================
+// 1. Intro Screen
+// ==========================
+export const firstHeading = "9 November, An Angel was sent to Earth 19 years Ago!"
+// Main heading text - Change this text as you like
+
+export const firstSubtext = "A girl with the softest heart was born "
+// Subtext under heading (optional) - Change, leave blank or remove.
+
+
+// ==========================
+// 2. Cake Screen
+// ==========================
+// Add recipient's name here.
+export const NAME = "Angel 🤍"
+
+
+// ==========================
+// 3. Photo Screen
+// ==========================
+export const photoScreenHeading = "Our small moments that never felt small"
+// Heading above photos - Change this text as you like
+
+// Add your photos inside "public/images" folder
+// Example: { "/images/1.jpeg" }, Match the format and add more as needed
+export const photos = [
+    "/images/1st.jpeg",
+    "/images/2nd.jpeg",
+    "/images/3rd.jpeg",
+    "/images/4th.jpeg",
+    "/images/5th.jpeg",
+]
+
+
+// ==========================
+// 4. Message Screen
+// ==========================
+export const messageScreenHeading = "A Note from KV"
+// Heading above card - Change this text as you like
+
+export const specialMessage = `Happy Birthday, Angel! 
+You are still just as beautiful as the parts of yourself you sometimes forget.
+I’ve seen you in every shade the laughing one, the exhausted one,
+the figuring-things-out one, the soft and quiet one and you were lovely in all of them.
+
+I remember the smallest things the most:
+the way you say “KV” with that tiny warmth,
+your cute “peet dungi!! kya hai!!” mock anger,
+your one-of-a-kind typos,
+and how you start singing without thinking.
+
+Those are my favorite things about you.
+Not big gestures. Not dramatic memories.
+Just the raw, real, everyday you.
+
+I only wish this for you:
+that your heart feels lighter this year.
+That you find peace.
+That you meet people who don’t just like you, but truly understand you.
+
+Thank you for crossing paths with my life the way you did.
+You’ll always hold a space in my heart and in my story. 🤍`
+// Main special message (you can edit freely)
+
+
+// ==========================
+// 4. Final Screen
+// ==========================
+export const finalScreenHeading = "One Last Thing..."
+// Heading above final surprise - Change this text as you like
+
+export const overlayText = "Lots of love for you Angel❤️ - From KV"
+// First text inside the overlay card - Change this text as you like
+
+export const overlayMessage = "Once again, Happy Birthday AG! Just stay soft. Stay yourself. Have fun girl 🤍🫶"
+
+// ==========================
+// Background Music
+// ==========================
+// Place your .mp3 file inside "public/audio" and update path below
+export const backgroundMusic = "./audio/bg.mp3"
